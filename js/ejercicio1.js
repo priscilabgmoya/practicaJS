6712cc1f7686c914c3ec2e32825471b2889b4f11
priscilaGarciaMoya <priscilagarciamoya@alu.frt.utn.edu.ar>
2023-02-26 11:51:20 -0300

    rama dev

--- js/ejercicio1.js
+++ js/ejercicio1.js
@@ -1,7 +1,5 @@
 /**
  * 1.- Escribe un programa de una sola línea que haga que aparezca en la pantalla un alert que diga “un mensaje”.
  */
-function mostrarAlerta(){
-    alert('un mensaje');
-}
+
 
