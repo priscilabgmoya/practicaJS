9a353ee984418a37c77add17d0d72d76646fd038
priscilaGarciaMoya <priscilagarciamoya@alu.frt.utn.edu.ar>
2023-02-26 17:39:27 -0300

    se agrega resolucion del ejercicio 10

--- js/ejercicio10.js
+++ js/ejercicio10.js
@@ -1,3 +1,24 @@
 /**
  * 10.- Escribe un programa que pida un número y nos diga si es divisible por 2, 3, 5 o 7 (sólo hay que comprobar si lo es por uno de los cuatro)
  */
+
+obtenerDivisible();
+
+function obtenerDivisible() {
+  var numero = prompt("ingrese un Numero A");
+
+  var element = parseInt(numero);
+
+  if (element % 2 === 0){
+    return document.write(`<p style= "color: black; font-size: 30px">  El Numeros A (${element})  <span style= "color: red; font-size: 30px" >Es divisible en 2 </span> </p>`);
+  }
+  if (element % 3 === 0){
+  return document.write(`<p style= "color: black; font-size: 30px">  El Numeros A (${element})  <span style= "color: red; font-size: 30px" >Es divisible en 3 </span> </p>`);
+  }
+  if (element % 5 === 0){
+    return document.write(`<p style= "color: black; font-size: 30px">  El Numeros A (${element})  <span style= "color: red; font-size: 30px" >Es divisible en 5 </span> </p>`);
+  }
+  if (element % 7 === 0){
+    return document.write(`<p style= "color: black; font-size: 30px">  El Numeros A (${element})  <span style= "color: red; font-size: 30px" >Es divisible en 7 </span> </p>`);
+  }
+}
