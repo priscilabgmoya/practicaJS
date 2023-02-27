689cf8be7287612cd61eff6128cf305e4278d00f
priscilaGarciaMoya <priscilagarciamoya@alu.frt.utn.edu.ar>
2023-02-27 19:53:18 -0300

    se modifico los parseint del ejercicio 8

--- js/ejercicio8.js
+++ js/ejercicio8.js
@@ -4,7 +4,7 @@
 esDivisibleEn2();
 
 function esDivisibleEn2(){
-    var numeroA = prompt("ingrese el valor del numero A"); 
+    var numeroA = parseInt(prompt("ingrese el valor del numero A")); 
     
 if(numeroA% 2 === 0 ){
     document.write(`<p style= "color: black; font-size: 30px">  El Numeros A (${numeroA})  <span style= "color: red; font-size: 30px" >Es divisible en 2 </span> </p>`)
