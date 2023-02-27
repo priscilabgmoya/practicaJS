d56f48df651867401e61b57c6f26519eabc9b51a
priscilaGarciaMoya <priscilagarciamoya@alu.frt.utn.edu.ar>
2023-02-27 19:35:55 -0300

    se modifico los parseint del ejercicio 6

--- js/ejercicio6.js
+++ js/ejercicio6.js
@@ -4,11 +4,11 @@
 calcularElMayorNumero(); 
 
 function calcularElMayorNumero(){
-var numeroA = prompt("ingrese el valor del numero A"); 
-var numeroB = prompt("ingrese el valor del numero B"); 
-if(parseInt(numeroA) > parseInt(numeroB)){
-document.write(`<p style= "color: black; font-size: 30px"> El mayor entre  los Numeros A (${numeroA}) y B (${numeroB}) es: <span style= "color: red; font-size: 30px" > ${(parseInt(numeroA))}</span> </p>`)
+var numeroA = parseInt(prompt("ingrese el valor del numero A")); 
+var numeroB = parseInt(prompt("ingrese el valor del numero B")); 
+if(numeroA > numeroB){
+document.write(`<p style= "color: black; font-size: 30px"> El mayor entre  los Numeros A (${numeroA}) y B (${numeroB}) es: <span style= "color: red; font-size: 30px" > ${(numeroA)}</span> </p>`)
 }else {
-    document.write(`<p style= "color: black; font-size: 30px"> El mayor entre  los Numeros A (${numeroA}) y B (${numeroB}) es: <span style= "color: red; font-size: 30px" > ${(parseInt(numeroB))}</span> </p>`)
+    document.write(`<p style= "color: black; font-size: 30px"> El mayor entre  los Numeros A (${numeroA}) y B (${numeroB}) es: <span style= "color: red; font-size: 30px" > ${(numeroB)}</span> </p>`)
 }
 }
